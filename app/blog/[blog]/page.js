6e026ff188dac1d9e880fec8a7b0ce4f17b41f21
Walid Daniel Dib <112762655+w-dib@users.noreply.github.com>
2024-01-07 22:01:32 +0400

    Fix typo

--- app/blog/[blog]/page.js
+++ app/blog/[blog]/page.js
@@ -18,31 +18,31 @@ export default async function BlogPage({ params }) {
   const blogs = categoriesedBlogs ? categoriesedBlogs : recentBlogs;
 
   return (
-    <article className='mx-auto p-4 bg-white flex-col items-center w-screen md:max-w-4xl h-full md:px-12'>
+    <article className="mx-auto p-4 bg-white flex-col items-center w-screen md:max-w-4xl h-full md:px-12">
       <BackButton />
 
-      <h1 className='my-2 text-2xl font-extrabold tracking-tight text-slate-900 md:text-4xl'>
+      <h1 className="my-2 text-2xl font-extrabold tracking-tight text-slate-900 md:text-4xl">
         {blog.title}
       </h1>
-      <p className='text-gray-500 text-sm'>
+      <p className="text-gray-500 text-sm">
         {formatPublishedDate(blog.publishedAt)}
       </p>
 
       <img
-        className='w-full h-96 object-contain rounded-md my-2'
+        className="w-full h-96 object-contain rounded-md my-2"
         src={blog.mainImage}
-        alt='Blog Image'
+        alt="Blog Image"
       />
 
-      <div className='px-2 prose prose-lg my-2text-justify mx-auto'>
+      <div className="px-2 prose prose-lg my-2text-justify mx-auto">
         <PortableText value={blog.body} components={RichTextComponents} />
       </div>
       {/* recent blogs */}
-      <div className='my-12 space-y-6'>
-        <h1 className='capitalize text-2xl'>
-          {categoriesedBlogs ? "similar" : "recent"} {"blog's"}
+      <div className="my-12 space-y-6">
+        <h1 className="capitalize text-2xl">
+          {categoriesedBlogs ? "similar" : "recent"} {"posts"}
         </h1>
-        <div className='flex flex-col gap-4 '>
+        <div className="flex flex-col gap-4 ">
           {blogs.map((post) => (
             <BlogCard key={post.id} post={post} />
           ))}
